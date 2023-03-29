f9b368ef97659460a64fd17d73b73895c631bf76
ultimatehecker <86735991+ultimatehecker@users.noreply.github.com>
2023-03-28 20:14:05 -0400

    added constants

---
+++ constants/constants.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+### Colors Constants ##
+ESC=$(printf '\033')
+RESET="${ESC}[0m" 
+DEFAULT="${ESC}[39m"
+RED="${ESC}[31m"
+GREEN="${ESC}[32m" 
+YELLOW="${ESC}[33m" 
+CYAN="${ESC}[36m" 
+BLUE="${ESC}[34m" 
+MAGENTA="${ESC}[35m"
+WHITE="${ESC}[37m"
+BLACK="${ESC}[30m" 
